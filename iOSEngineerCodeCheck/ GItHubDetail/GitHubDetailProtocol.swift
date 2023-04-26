bfc83d0eae556f533f467d570ee08db2f94fdfa2
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-27 05:27:29 +0900

    add: 各要素のプロトコルの定義

--- iOSEngineerCodeCheck/ GItHubDetail/GitHubDetailProtocol.swift
+++ iOSEngineerCodeCheck/ GItHubDetail/GitHubDetailProtocol.swift
@@ -1,1 +1,14 @@
-import Foundation
+import UIKit
+// View
+protocol GitHubDetailView: AnyObject {
+    var presenter: GitHubDetailPresenter { get }
+}
+// Presentation
+protocol GitHubDetailPresentation: AnyObject {
+    var view: GitHubDetailView? { get }
+    var router: GitHubDetailRouter? { get }
+}
+// Router
+protocol GitHubDetailWireFrame: AnyObject {
+    static func assembleModules(gitHub: User) -> UIViewController
+}
