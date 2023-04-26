5d4e460f69100d86a8344e6f77449b6342bf3e87
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-27 00:40:35 +0900

    add: Utility デバック機能作成

--- iOSEngineerCodeCheck/Utility/Debug.swift
+++ iOSEngineerCodeCheck/Utility/Debug.swift
@@ -1,9 +1,15 @@
-//
-//  Debug.swift
-//  iOSEngineerCodeCheck
-//
-//  Created by 日高隼人 on 2023/04/27.
-//  Copyright © 2023 YUMEMI Inc. All rights reserved.
-//
-
 import Foundation
+// swiftlint:disable all
+struct Debug {
+    static func log(_ obj: Any? = nil, file: String = #file, function: String = #function, line: Int = #line, errorDescription: String) {
+        var filename: NSString = file as NSString
+        filename = filename.lastPathComponent as NSString
+        let text: String
+        if let obj = obj {
+            text = "[File: \(filename) Func: \(function) Line: \(line)] : \(obj)"
+        } else {
+            text = "[File: \(filename) Func: \(function) Line:\(line)]"
+        }
+        print(text, "✨💀", errorDescription)
+    }
+}
