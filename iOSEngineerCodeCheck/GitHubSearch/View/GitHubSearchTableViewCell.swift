a562a84fbb741c1c012090597dcd24f73f1f3c4a
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-26 23:04:30 +0900

    add: identifier記述

--- iOSEngineerCodeCheck/GitHubSearch/View/GitHubSearchTableViewCell.swift
+++ iOSEngineerCodeCheck/GitHubSearch/View/GitHubSearchTableViewCell.swift
@@ -3,6 +3,7 @@ import UIKit
 final class GitHubSearchTableViewCell: UITableViewCell {
     @IBOutlet private weak var fullNameLabel: UILabel!
     @IBOutlet private weak var languageLabel: UILabel!
+    static let identifier = "GitHubSearchCell"
 
     func configure(fullName: String, language: String) {
         fullNameLabel.text = fullName
