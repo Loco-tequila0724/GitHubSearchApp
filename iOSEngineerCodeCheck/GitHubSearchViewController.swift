0642fe2e9dd4abd5c9eb4fc7f11710ad3fad283f
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-26 12:55:42 +0900

    add: GitHubデータ構造 修正

--- iOSEngineerCodeCheck/GitHubSearchViewController.swift
+++ iOSEngineerCodeCheck/GitHubSearchViewController.swift
@@ -1,32 +1,30 @@
 import UIKit
 
-struct GitHub {
-    var items: [User]
+struct GitHubRepository: Decodable {
+    var items: [User]?
 }
 
-struct User {
-    let language: String
-    let stargazersCount: String
-    let wachersCount: String
-    let forks_count: String
-    let openIssuesCount: String
-    let full_name: String
-
-    let owner: [Owner]
+struct User: Decodable {
+    let fullName: String
+    let language: String?
+    let stargazersCount: Int
+    let watchersCount: Int
+    let forksCount: Int
+    let openIssuesCount: Int
+    let owner: Owner
 
     enum CodingKeys: String, CodingKey {
         case language = "language"
         case stargazersCount = "stargazers_count"
-        case wachersCount = "wachers_count"
+        case watchersCount = "watchers_count"
         case forksCount = "forks_count"
         case openIssuesCount = "open_issues_count"
         case fullName = "full_name"
-        case owner = "owner"
-
+        case owner
     }
 
-    struct Owner {
-        let avatar_url: String
+    struct Owner: Decodable {
+        let avatarUrl: URL
 
         enum CodingKeys: String, CodingKey {
             case avatarUrl = "avatar_url"
@@ -39,6 +37,8 @@ class GitHubSearchViewController: UITableViewController, UISearchBarDelegate {
     var repository: [[String: Any]] = []
     var task: URLSessionTask?
     var tappedRow: Int?
+    private let decoder = JSONDecoder()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -53,24 +53,52 @@ class GitHubSearchViewController: UITableViewController, UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         task?.cancel()
     }
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        Task {
+            await fetchGitHubData()
+        }
+
+    }
+
+    func fetchGitHubData() async {
         guard let text = searchBar.text, !text.isEmpty else { return }
         guard let url: URL = URL(string: "https://api.github.com/search/repositories?q=\(text)") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
 
-        task = URLSession.shared.dataTask(with: url) { (data, _, _) in
-            guard let data else { return }
-            if let obj = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
-                if let items = obj["items"] as? [[String: Any]] {
-                    self.repository = items
-
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                }
-            }
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            print("●～*")
+            guard let httpResponse = response as? HTTPURLResponse,
+                httpResponse.statusCode == 200 else { return }
+            print("●～* 1")
+            let gitHubRepository = try decoder.decode(GitHubRepository.self, from: data)
+            print("●～* 2")
+            print(gitHubRepository)
+        } catch let error {
+            print(error)
         }
-        task?.resume()
     }
+
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        guard let text = searchBar.text, !text.isEmpty else { return }
+//        guard let url: URL = URL(string: "https://api.github.com/search/repositories?q=\(text)") else { return }
+//
+//        task = URLSession.shared.dataTask(with: url) { (data, _, _) in
+//            guard let data else { return }
+//            if let obj = try? JSONSerialization.jsonObject(with: data) as? [String: Any] {
+//                if let items = obj["items"] as? [[String: Any]] {
+//                    self.repository = items
+//
+//                    DispatchQueue.main.async {
+//                        self.tableView.reloadData()
+//                    }
+//                }
+//            }
+//        }
+//        task?.resume()
+//    }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Detail" {
@@ -78,9 +106,11 @@ class GitHubSearchViewController: UITableViewController, UISearchBarDelegate {
             gitHubDetailVC?.gitHubSearchVC = self
         }
     }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return repository.count
     }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         let repository = repository[indexPath.row]
@@ -89,6 +119,7 @@ class GitHubSearchViewController: UITableViewController, UISearchBarDelegate {
         cell.tag = indexPath.row
         return cell
     }
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
         tappedRow = indexPath.row
