c23cd388af5378fb039e189b59eb9312812248d6
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-26 21:10:41 +0900

    add: 各要素のプロトコルを定義

--- iOSEngineerCodeCheck/GitHubSearch/GitHubSearchProtocol.swift
+++ iOSEngineerCodeCheck/GitHubSearch/GitHubSearchProtocol.swift
@@ -1,9 +1,38 @@
-//
-//  GitHubSearchProtocol.swift
-//  iOSEngineerCodeCheck
-//
-//  Created by 日高隼人 on 2023/04/26.
-//  Copyright © 2023 YUMEMI Inc. All rights reserved.
-//
-
 import Foundation
+// View
+protocol GitHubSearchView {
+    var presenter: GitHubPresentation { get }
+    func configure()
+    func startLoading()
+    func stopLoading()
+    func tableViewReload()
+}
+// Presenter
+protocol GitHubPresentation {
+    var view: GitHubSearchViewController? { get }
+    var interactor: GitHubSearchInputUsecase { get }
+    var router: GitHubSearchWireFrame { get }
+//    var gitHubRepository: [] { get }
+    func viewDidLoad()
+    /// サーチボタンのタップ通知
+    func searchButtonDidPush()
+    /// キャンセルボタンのタップ通知
+    func cancelButtonDidPush()
+    /// セルタップを通知
+    func didSelectRow()
+}
+// Interactor インプット
+protocol GitHubSearchInputUsecase {
+    var presenter: GitHubSearchOutputUsecase? { get }
+    /// API通信を行い、GitHubのデータを取得
+    func fetchGitHubData(text: String)
+}
+// Interactor アウトプット
+protocol GitHubSearchOutputUsecase {
+//    func didFetchGitHubResult(result: Result<[], erorr>)
+}
+// Router
+protocol GitHubSearchWireFrame {
+    static func assembleModules()
+    func showGitHubDetailVC()
+}
