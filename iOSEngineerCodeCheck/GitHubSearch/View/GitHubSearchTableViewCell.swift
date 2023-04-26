49d13b639d6b48482a57400d1ee0ea79f7df14f7
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-26 22:35:23 +0900

    add:  テーブルビューカスタムセル実装

--- iOSEngineerCodeCheck/GitHubSearch/View/GitHubSearchTableViewCell.swift
+++ iOSEngineerCodeCheck/GitHubSearch/View/GitHubSearchTableViewCell.swift
@@ -1,5 +1,11 @@
 import UIKit
 
-class GitHubSearchTableViewCell: UITableViewCell {
+final class GitHubSearchTableViewCell: UITableViewCell {
+    @IBOutlet private weak var fullNameLabel: UILabel!
+    @IBOutlet private weak var languageLabel: UILabel!
 
+    func configure(fullName: String, language: String) {
+        fullNameLabel.text = fullName
+        languageLabel.text = language
+    }
 }
