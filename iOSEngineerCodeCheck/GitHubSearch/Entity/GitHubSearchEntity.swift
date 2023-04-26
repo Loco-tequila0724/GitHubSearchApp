16b7d674cf1a245a9176e7206c61ab4b015a7457
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-26 21:58:51 +0900

    add: GitHubSearchのEntityを作成。

--- iOSEngineerCodeCheck/GitHubSearch/Entity/GitHubSearchEntity.swift
+++ iOSEngineerCodeCheck/GitHubSearch/Entity/GitHubSearchEntity.swift
@@ -1,5 +1,33 @@
 import Foundation
 
-struct GitHubSearchEntity {
-    
+struct GitHubSearchEntity: Decodable {
+    var items: [User]?
+}
+
+struct User: Decodable {
+    let fullName: String
+    let language: String?
+    let stargazersCount: Int
+    let watchersCount: Int
+    let forksCount: Int
+    let openIssuesCount: Int
+    let owner: Owner
+
+    enum CodingKeys: String, CodingKey {
+        case language = "language"
+        case stargazersCount = "stargazers_count"
+        case watchersCount = "watchers_count"
+        case forksCount = "forks_count"
+        case openIssuesCount = "open_issues_count"
+        case fullName = "full_name"
+        case owner
+    }
+}
+
+struct Owner: Decodable {
+    let avatarUrl: URL
+
+    enum CodingKeys: String, CodingKey {
+        case avatarUrl = "avatar_url"
+    }
 }
