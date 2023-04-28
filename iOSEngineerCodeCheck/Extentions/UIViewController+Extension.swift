231c8aea8f2599e8702c973af00b07460d02ffad
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-04-28 20:16:32 +0900

    add : ナビゲーションバー作成の関数を定義

--- iOSEngineerCodeCheck/Extentions/UIViewController+Extension.swift
+++ iOSEngineerCodeCheck/Extentions/UIViewController+Extension.swift
@@ -13,4 +13,24 @@ extension UIViewController {
             self.present(alert, animated: true, completion: nil)
         }
     }
+
+    func setupNavigationBar(title: String, fontSize: CGFloat = 18) {
+        navigationItem.title = title
+
+        navigationItem.backBarButtonItem =
+            UIBarButtonItem(
+            title: "",
+            style: .plain,
+            target: nil,
+            action: nil)
+        navigationItem.backBarButtonItem?.tintColor = .white
+
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        //        appearance.backgroundColor = #colorLiteral(red: 0.1089240834, green: 0.09241241962, blue: 0.1406958103, alpha: 1)
+        appearance.backgroundColor = .black
+        appearance.titleTextAttributes = [.font: UIFont.systemFont(ofSize: fontSize, weight: .bold), .foregroundColor: UIColor.white]
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
+    }
 }
