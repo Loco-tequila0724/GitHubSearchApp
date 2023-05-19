20e5bd8c820fd316fec7342eb372e204512c4499
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-05-19 15:46:35 +0900

    add: StarOderのデータ構造を作成

--- iOSEngineerCodeCheck/Utilities/Entity/StarOrder.swift
+++ iOSEngineerCodeCheck/Utilities/Entity/StarOrder.swift
@@ -30,3 +30,45 @@ enum StarOrder {
         }
     }
 }
+
+protocol StarOrderTest {
+    var items: [Item?] { get }
+    var text: String { get }
+    var color: UIColor { get }
+}
+
+struct StarDesc: StarOrderTest {
+    var items: [Item?]
+    var text: String
+    var color: UIColor
+
+    init() {
+        self.items = []
+        self.text = "☆ Star数 ⍋"
+        self.color = #colorLiteral(red: 0.1634489, green: 0.1312818527, blue: 0.2882181406, alpha: 1)
+    }
+}
+
+struct StarAsc: StarOrderTest {
+    var items: [Item?]
+    var text: String
+    var color: UIColor
+
+    init() {
+        self.items = []
+        self.text = "☆ Star数 ⍒"
+        self.color = #colorLiteral(red: 0.1634489, green: 0.1312818527, blue: 0.2882181406, alpha: 1)
+    }
+}
+
+struct StarDefault: StarOrderTest {
+    var items: [Item?]
+    var text: String
+    var color: UIColor
+
+    init() {
+        self.items = []
+        self.text = "☆ Star数 "
+        self.color = .lightGray
+    }
+}
