8b1a5eab9d4de0a4a772af4b0a7e981198142948
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-05-21 00:26:22 +0900

    add: ImageViewファイル追加

--- iOSEngineerCodeCheck/GitHubSearch/View/AvatarImageView.swift
+++ iOSEngineerCodeCheck/GitHubSearch/View/AvatarImageView.swift
@@ -1,1 +1,21 @@
+//
+//  AvatarImageView.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 日高隼人 on 2023/05/21.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
 
+import UIKit
+
+class AvatarImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
