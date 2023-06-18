7af747e53c5fdae11cf4fcc8cf1824c54fc34887
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-06-19 04:54:37 +0900

    add: UITableView エクステンション 作成

--- iOSEngineerCodeCheck/Extentions/UITableView+Extension.swift
+++ iOSEngineerCodeCheck/Extentions/UITableView+Extension.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2023 YUMEMI Inc. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    func dequeue<Cell: UITableViewCell>(_ cell: Cell.Type = Cell.self, with identifier: String? = nil, for indexPath: IndexPath) -> Cell {
+        let identifier = identifier ?? String(describing: Cell.self)
+        let cell = dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? Cell
+        return cell ?? {
+            fatalError("\(Cell.self) not registered.")
+        }()
+    }
+
+    func cellForRow<Cell: UITableViewCell>(at indexPath: IndexPath, _ type: Cell.Type = Cell.self) -> Cell? {
+        cellForRow(at: indexPath) as? Cell
+    }
+}
