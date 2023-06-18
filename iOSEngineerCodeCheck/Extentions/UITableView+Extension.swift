315f83d7cbf1563d983dd43c42b5bb596a1c1376
Loco-tequila0724 <hayato09097699090@icloud.com>
2023-06-19 04:53:10 +0900

    add: TableViewDataSource 作成

---
+++ iOSEngineerCodeCheck/Extentions/UITableView+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UITableView+Extension.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 日高隼人 on 2023/06/18.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
--- iOSEngineerCodeCheck/GitHubSearch/View/GitHubSearchTableViewDataSource.swift
+++ iOSEngineerCodeCheck/GitHubSearch/View/GitHubSearchTableViewDataSource.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2023 YUMEMI Inc. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@MainActor
+final class GitHubSearchTableViewDataSource: NSObject {
+    typealias TableRow = GitHubSearchViewItem.TableRow
+
+    private unowned let tableView: UITableView
+    private var items: [TableRow] = []
+
+    init(tableView: UITableView) {
+        self.tableView = tableView
+    }
+
+    func reload(with items: [TableRow]) {
+        self.items = items
+        self.tableView.reloadData()
+    }
+
+    func replace(item: TableRow, at index: Int) {
+        guard index < items.count, items[index].id == item.id else { return }
+        items[index] = item
+        let indexPath = IndexPath(row: index, section: 0)
+        if let cell: GitHubSearchTableViewCell = tableView.cellForRow(at: indexPath) {
+            cell.configure(item: item)
+        }
+    }
+}
+
+extension GitHubSearchTableViewDataSource: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = items[indexPath.row]
+        let cell: GitHubSearchTableViewCell = tableView.dequeue(for: indexPath)
+        cell.configure(item: item)
+        return cell
+    }
+}
